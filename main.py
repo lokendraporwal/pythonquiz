83cd888571ae94dd18f7a9f5fe3fee3ce6feeb6b
Lokendra Kumar Ved <lokendra.ved63@gmail.com>
2019-08-23 13:44:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,20 @@
 import json
-n=int(input("Enter 1 for sports \n Enter 2 for Maths : "))
 with open("quiz.json",'r') as f:
     dict=json.load(f)
-if n==1:
-    result = 0;
-    for j in dict['quiz']['sport']:
-            i = 1
-            que = dict['quiz']['sport'][j]['question']
-            options = dict['quiz']['sport'][j]['options']
-            print(que)
-            for k in options:
-                print(i, k)
-                i = i + 1
-            ans = int(input("Your Answer(1/2/3/4) : "))
-            if (options[ans - 1] == dict['quiz']['sport'][j]['answer']):
-                result = result + 1
-    print("Your result is : ", result)
-if(n==2):
-    result = 0;
-    for j in dict['quiz']['maths']:
-            i = 1
-            que = dict['quiz']['maths'][j]['question']
-            options = dict['quiz']['maths'][j]['options']
-            print(que)
-            for k in options:
-                print(i, k)
-                i = i + 1
-            ans = int(input("Your Answer(1/2/3/4) : "))
-            if (options[ans - 1] == dict['quiz']['maths'][j]['answer']):
-                result = result + 1
-    print("Your result is : ", result)
+result = 0
+for j in dict['quiz']:
+    print(j)
+n=input("Enter Any one of the above : ")
+result=0
+for i in dict['quiz'][n]:
+    que=dict['quiz'][n][i]['question']
+    option=dict['quiz'][n][i]['options']
+    print(que)
+    index=1
+    for k in option:
+        print(index,k)
+        index=index+1
+    ans=int(input("Enter Index of above Option to answer : "))
+    if(option[ans-1]==dict['quiz'][n][i]['answer']):
+        result=result+1
+print("Your Result is : ", result)
